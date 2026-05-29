77988999261a38d1446fa8fca849c05d46ce8214
CHRISMIKE <etae33619@gmail.com>
2026-05-28 22:32:01 -0700

    Add explicit CORS headers to flask response routes

--- SEVER/server.py
+++ SEVER/server.py
@@ -13,6 +13,7 @@ def get_location_names():
     response = jsonify({
         'locations': util.get_location_names()
     })
+    response.headers.add('Access-Control-Allow-Origin', '*')
     return response
 
 
@@ -26,6 +27,7 @@ def predict_home_price():
     response = jsonify({
         'estimated_price': util.get_estimated_price(location, total_sqft, bhk, bath)
     })
+    response.headers.add('Access-Control-Allow-Origin', '*')
 
     return response  # Added the crucial missing return statement!
 
